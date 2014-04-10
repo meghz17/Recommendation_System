566d084694ac14a4e77f734ef933d5bee2eadd04
meghz17 <meghz17@neo.tamu.edu>
2014-04-10 12:59:07 -0500

    Update logout.php
    I have edited the file accordingly.

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@
 
     session_destroy();
 
-    echo "You are logged out. <a href='index.php'>Click</a> here to return.";
+    echo "You are logged out. <a href='index.php'>Click</a> here to return..";
 
 ?>
